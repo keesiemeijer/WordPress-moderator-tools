11612258ace5a63bf4cdd7aa9a0a70c6eac6d16c
keesiemeijer <keesie.meijer@gmail.com>
2014-03-24 20:02:08 +0100

    formatted

--- version-2/wp-topic-redirect.user.js
+++ version-2/wp-topic-redirect.user.js
@@ -10,28 +10,25 @@
 
 ( function() {
 
-    var search = window.location.search;
-    var https_protocol = window.location.protocol.replace(/http\:/g, 'https:');
-    var url = https_protocol + "//" + window.location.host + window.location.pathname;
-    var new_url = (https_protocol != window.location.protocol) ? url : false;
+	var search = window.location.search;
+	var https_protocol = window.location.protocol.replace( /http\:/g, 'https:' );
+	var url = https_protocol + "//" + window.location.host + window.location.pathname;
+	var new_url = ( https_protocol != window.location.protocol ) ? url : false;
 
-    if ( search ) {
+	if ( search ) {
 
-        pattern = /view=all/g;
+		pattern = /view=all/g;
 
-        if ( !pattern.test( search ) ) {
-            new_url = ((new_url) ? new_url : url) + search + '&view=all';
-        }
+		if ( !pattern.test( search ) ) {
+			new_url = ( ( new_url ) ? new_url : url ) + search + '&view=all';
+		}
 
-    } else {
-        new_url = ((new_url) ? new_url : url) + '?view=all';
-    }
+	} else {
+		new_url = ( ( new_url ) ? new_url : url ) + '?view=all';
+	}
 
-    if ( new_url ) {
-       
-            window.location.replace( new_url + window.location.hash);
-        
-        
-    }
+	if ( new_url ) {
+		window.location.replace( new_url + window.location.hash );
+	}
 
 } )();
